4ab15e0a7496fdd04aa27bd402cb965db6afe0d0
rdm10br <43587845+rdm10br@users.noreply.github.com>
2019-12-03 17:28:59 -0300

    att

--- LOTv3/app/src/main/java/com/example/lotv3/ChatActivity.java
+++ LOTv3/app/src/main/java/com/example/lotv3/ChatActivity.java
@@ -3,6 +3,8 @@ package com.example.lotv3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class ChatActivity extends AppCompatActivity
 {
@@ -12,5 +14,15 @@ public class ChatActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+    }
+    public void onSend(View view)
+    {
+        alert("teste 1");
+        alert("teste 2");
+    }
+
+    public void alert(String msg)
+    {
+        Toast.makeText(this,msg, Toast.LENGTH_LONG).show();
     }
 }
