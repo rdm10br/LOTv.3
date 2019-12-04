773da3f437a4694567bd6ef23ddc7607ce019e22
ed202012 <50635673+ed202012@users.noreply.github.com>
2019-12-04 02:12:08 -0300

    snapshot

--- LOTv3/app/src/main/java/com/example/lotv3/ChatActivity.java
+++ LOTv3/app/src/main/java/com/example/lotv3/ChatActivity.java
@@ -1,5 +1,6 @@
 package com.example.lotv3;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,11 +13,24 @@ import android.widget.Toast;
 import com.example.lotv3.util.Mensagem;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.lang.ref.Reference;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity
 {
 
+    private List<Mensagem>mChat;
     private FirebaseUser Userid = FirebaseAuth.getInstance().getCurrentUser();
     String Uid = Userid.getUid();
     private FirebaseFirestore DB = FirebaseFirestore.getInstance();
@@ -28,7 +42,27 @@ public class ChatActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+
     }
+
+    private void readMessage(){
+        mChat = new ArrayList<>();
+
+        final DocumentReference msg = DB.collection("Mensagens").document();
+        msg.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                if (e !=null){
+                    alert("falha");
+                    return;
+                }
+                if (documentSnapshot !=null && documentSnapshot.exists()){
+                    mChat
+                }
+            }
+        });
+    }
+
     public void onSend(View view)
     {
         TextView msgEdt = findViewById(R.id.messageET);
@@ -42,6 +76,8 @@ public class ChatActivity extends AppCompatActivity
         alert("teste 1");
 
     }
+
+
 
 
 
