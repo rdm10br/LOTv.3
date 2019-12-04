92a06f37b2b078df0a2eabe725a40cda725f2848
ed202012 <50635673+ed202012@users.noreply.github.com>
2019-12-04 02:48:44 -0300

    snapshot

--- LOTv3/app/src/main/java/com/example/lotv3/ChatActivity.java
+++ LOTv3/app/src/main/java/com/example/lotv3/ChatActivity.java
@@ -1,5 +1,6 @@
 package com.example.lotv3;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +14,11 @@ import android.widget.Toast;
 import com.example.lotv3.util.Mensagem;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -29,6 +35,7 @@ import java.util.List;
 
 public class ChatActivity extends AppCompatActivity
 {
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
     private List<Mensagem>mChat;
     private FirebaseUser Userid = FirebaseAuth.getInstance().getCurrentUser();
@@ -48,19 +55,25 @@ public class ChatActivity extends AppCompatActivity
     private void readMessage(){
         mChat = new ArrayList<>();
 
-        final DocumentReference msg = DB.collection("Mensagens").document();
-        msg.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+        reference= FirebaseDatabase.getInstance().getReference("Mensagens");
+        reference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-                if (e !=null){
-                    alert("falha");
-                    return;
-                }
-                if (documentSnapshot !=null && documentSnapshot.exists()){
-                    mChat
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mChat.clear();
+                for (DataSnapshot snapshot :dataSnapshot.getChildren()){
+                    Mensagem mensagem = snapshot.getValue(Mensagem.class);
+                    if (mensagem.getUsuario().equals(Uid)&& mensagem.getSender().equals(Userid)
+                    ||mensagem.getUsuario().equals(Userid)&& mensagem.getSender().equals(Uid)){
+                     mChat
+                    }
                 }
             }
-        });
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        })
     }
 
     public void onSend(View view)
